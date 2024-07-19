c1439df2230251103a9762e66ffabe9e4c19edee
luisrivas123 <luis.rivas@correounivalle.edu.co>
2024-07-19 20:49:00 +0200

    tailwind, score component

--- components/GameCard.jsx
+++ components/GameCard.jsx
@@ -1,13 +1,23 @@
 import { StyleSheet, Text, Image, View, Animated } from "react-native";
 import { useEffect, useRef } from "react";
+import { Score } from "./Score";
 
 export function GameCard({ game }) {
   return (
-    <View key={game.slug} style={styles.card}>
+    <View
+      className="flex-row bg-slate-500/10 p-4 rounded-xl gap-4 mb-10"
+      key={game.slug}
+    >
       <Image source={{ uri: game.image }} style={styles.image} />
-      <Text style={styles.title}>{game.title}</Text>
-      <Text style={styles.score}>{game.score}</Text>
-      <Text style={styles.description}>{game.description}</Text>
+      <View>
+        <Text className="mb-1" style={styles.title}>
+          {game.title}
+        </Text>
+        <Score score={game.score} maxScore={100} />
+        <Text className="mt-2 flex-shrink" style={styles.description}>
+          {game.description.slice(0, 100)}...
+        </Text>
+      </View>
     </View>
   );
 }
