807a174c8ca42a2db24d5a5c50bc6b11e51d02dd
luisrivas123 <luis.rivas@correounivalle.edu.co>
2024-07-19 19:08:51 +0200

    First commit

---
+++ components/Main.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { FlatList, View, ActivityIndicator } from "react-native";
+import { getLatestGames } from "../lib/metacritic.js";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AnimatedGameCard } from "./GameCard.jsx";
+import { Logo } from "./Logo.jsx";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20, marginTop: 10 }}>
+        <Logo />
+      </View>
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
